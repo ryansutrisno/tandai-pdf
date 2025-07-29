5061ee660ba62f30f7b0a6840c825ab67731ebd3
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2025-07-29 02:06:39 +0000

    ketika diakses di browser mobile kenapa tidak ada notifikasi instalasi P

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: false,
 });
 
 const nextConfig: NextConfig = {
